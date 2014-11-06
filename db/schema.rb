3fc0d49c3fce9a2768b95b5da6754be7d11749be
Quincy Iheme <qiheme@gmail.com>
2014-11-06 12:30:28 -0500

    switched db to postgres from sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141106155438) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "crime_reports", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
